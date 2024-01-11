fab4101702bca8af69a9de5cf2063f253339c9ce
Ulriche Nguetsa <ulrichenguetsa6@gmail.com>
2024-01-11 03:12:00 +0100

    the first deployal of my portfolio website online

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.grid-item img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup').style.display= 'block';
+        document.querySelector('.popup img').src= image.getAttribute('src');
+    }
+});
+document.querySelector('.popup span').onclick =() =>{
+    document.querySelector('.popup').style.display= 'none';
+}
